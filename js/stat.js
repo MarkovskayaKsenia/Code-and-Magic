13280a1a10d105b4bf2be4b68d4d5d01bd52cb20
lubamark <lubamark@mail.ru>
2018-06-04 20:13:54 +0700

    add renderColumn function

--- js/stat.js
+++ js/stat.js
@@ -10,9 +10,8 @@ var INNER_GAP_X = 40;
 var LINE_HEIGHT = 18;
 
 var BAR_HEIGHT = 150;
-var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-
+var COLUMN_WIDTH = 40;
 
 var USER_COLOR = 'rgba(255, 0, 0, 1)';
 var getRandomBlue = function () {
@@ -36,6 +35,20 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var renderColumn = function (ctx, players, times, index) {
+  var columnHeight = (BAR_HEIGHT * times[index]) / getMaxElement(times);
+  var columnGap = COLUMN_WIDTH + BAR_GAP;
+  var columnY = CLOUD_Y + INNER_GAP_Y + LINE_HEIGHT * 3 + (BAR_HEIGHT - columnHeight);
+  var columnX = CLOUD_X + INNER_GAP_X + columnGap * index;
+
+  ctx.fillRect(columnX, columnY, COLUMN_WIDTH, columnHeight);
+
+  ctx.fillStyle = '#000';
+  ctx.textBaseline = 'Hanging';
+  ctx.fillText(players[index], columnX, CLOUD_Y + CLOUD_HEIGHT - INNER_GAP_Y);
+  ctx.fillText(Math.round(times[index]), columnX, columnY - GAP);
+};
+
 window.renderStatistics = function (ctx, players, times) {
 
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
@@ -46,24 +59,12 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText('Ура, вы победили', CLOUD_X + INNER_GAP_X, CLOUD_Y + INNER_GAP_Y);
   ctx.fillText('Список результатов:', CLOUD_X + INNER_GAP_X, CLOUD_Y + INNER_GAP_Y + LINE_HEIGHT);
 
-  var maxTime = getMaxElement(times);
-
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = getRandomBlue();
     if (players[i] === 'Вы') {
       ctx.fillStyle = USER_COLOR;
     }
 
-    var columnHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    var columnGap = BAR_WIDTH + BAR_GAP;
-    var columnY = CLOUD_Y + INNER_GAP_Y + LINE_HEIGHT * 3 + (BAR_HEIGHT - columnHeight);
-    var columnX = CLOUD_X + INNER_GAP_X + columnGap * i;
-
-    ctx.fillRect(columnX, columnY, BAR_WIDTH, columnHeight);
-
-    ctx.fillStyle = '#000';
-    ctx.textBaseline = 'Hanging';
-    ctx.fillText(players[i], columnX, CLOUD_Y + CLOUD_HEIGHT - INNER_GAP_Y);
-    ctx.fillText(Math.round(times[i]), columnX, columnY - GAP);
+    renderColumn(ctx, players, times, i);
   }
 };
