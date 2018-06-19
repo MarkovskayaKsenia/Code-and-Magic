ea52339536514b06a034a71a68be6133c6b2ba66
lubamark <lubamark@mail.ru>
2018-06-20 01:55:06 +0700

    add draggable + reset coords

--- js/setup.js
+++ js/setup.js
@@ -110,12 +110,13 @@ similarWizardList.appendChild(fragment);
 var similarWizardsWindow = document.querySelector('.setup-similar');
 similarWizardsWindow.classList.remove('hidden');
 
-
 // Находим блок, который надо показать, кнопку открытия, кнопку закрытия
 var userDialog = document.querySelector('.setup');
 var userDialogOpen = document.querySelector('.setup-open-icon');
-var userDialogClose = document.querySelector('.setup-close');
-var userNameInput = document.querySelector('.setup-user-name');
+var userDialogClose = userDialog.querySelector('.setup-close');
+var userNameInput = userDialog.querySelector('.setup-user-name');
+var userDialogStyleLeft = userDialog.style.left;
+var userDialogStyleTop = userDialog.style.top;
 var ESC_CODE = 27;
 var ENTER_CODE = 13;
 
@@ -130,15 +131,19 @@ var onDialogEscPress = function (evt) {
     }
   }
 };
+var resetDialogCoords = function () {
+  userDialog.style.left = userDialogStyleLeft;
+  userDialog.style.top = userDialogStyleTop;
+};
 var openDialog = function () {
   userDialog.classList.remove('hidden');
+  resetDialogCoords();
   document.addEventListener('keydown', onDialogEscPress);
 };
 var closeDialog = function () {
   userDialog.classList.add('hidden');
   document.addEventListener('keydown', onDialogEscPress);
 };
-
 
 // Обработчики событий для открывания окна
 userDialogOpen.addEventListener('click', openDialog);
@@ -191,4 +196,51 @@ wizardFireball.addEventListener('click', function () {
   }
   wizardFireball.style.background = newColor;
   wizardFireballInput.value = newColor;
+});
+
+
+// Добавляем draggable аватару
+var userDialogHandler = userDialog.querySelector('.upload');
+userDialogHandler.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        userDialogHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      userDialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
